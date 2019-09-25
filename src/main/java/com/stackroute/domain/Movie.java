71068731ec0d9e26b52212975df989356ead11fa
avinash <avinashchandrakar04@gmail.com>
2019-09-25 16:36:29 +0530

    Aware interface Commit

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,11 +1,17 @@
 package com.stackroute.domain;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.stereotype.Component;
 
 @Component
-public class Movie {
+public class Movie implements BeanNameAware, BeanFactoryAware, ApplicationContextAware {
     Actor actor;
 
     public Actor getActor() {
@@ -25,4 +31,18 @@ public class Movie {
         actor.display();
     }
 
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("Inside Bean Factory Aware");
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("Inside Bean Name Aware");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        System.out.println("Inside Application Context Aware");
+    }
 }
